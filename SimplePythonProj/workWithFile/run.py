ffb5ddd6d6a68728838f818a872db0f5bd750107
Asp772 <Kamarou_S@IBA>
2018-05-10 14:00:53 +0300

    next

---
+++ SimplePythonProj/workWithFile/run.py
@@ -0,0 +1,26 @@
+import wfimp
+import msvcrt as m
+#from wfimp import FileWork
+def testFunc():
+    for i in 'hello world':
+        if i == 'o':
+            continue
+        print(i * 2, end='')
+    A = 't' if 'spam' else 'f'
+    print("\n",A)
+
+fl = wfimp.FileWork()
+fl.setOpenType('w')
+fl.writeFunc('somestr',3)
+fl.readFunc()
+fl.setOpenType('w')
+fl.writeFunc('somestr',3)
+fl.readFunc()
+
+wfimp.readFile2()
+
+
+
+def wait():
+    m.getch()
+wait()
